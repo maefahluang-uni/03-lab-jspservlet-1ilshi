771922073466ea055e02e8f9eae85fb3d8dc583a
Muhammadrasheed Rida Ahmad Unus Ahmad Samadi <105810480+1ilshi@users.noreply.github.com>
2023-09-04 09:32:11 +0000

    done

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -1,7 +1,6 @@
 package th.mfu;
 
 import java.io.IOException;
-import Math;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -17,30 +16,31 @@ public class BMICalculatorServlet extends HttpServlet{
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //TODO: get parameter from request: "weight" and "height"
         int weight = Integer.parseInt(request.getParameter("weight"));
-        int height = Integer.parseInt(request.getParameter("height"));
+        Double height = Double.parseDouble(request.getParameter("height"));
         
         //TODO: calculate bmi
-        int BMI = weight/(height*height);
-        BMI = Math.round(BMI);
+        Double BMI = weight/(height*height);
+        Math.round(BMI);
 
         //TODO: determine the built from BMI
-        String builtType(){
-            if(BMI < 18.5){
-                return "underweight";
-            }else if(BMI >= 18.5 && BMI <25){
-                return "normal";
-            }else if(BMI >= 25 && BMI < 30){
-                return "overweight";
-            }else if(BMI >= 30 && BMI < 35){
-                return "obese";
-            }else if(BMI >= 35){
-                return "extremely obese"
-            }
-        }        
+        String builtType;
+        if(BMI < 18.5){
+            builtType = "underweight";
+        }else if(BMI >= 18.5 && BMI <25){
+            builtType = "normal";
+        }else if(BMI >= 25 && BMI < 30){
+            builtType = "overweight";
+        }else if(BMI >= 30 && BMI < 35){
+            builtType = "obese";
+        }else{
+            builtType = "extremely obese";
+        }
+            
+             
       
         //TODO: add bmi and built to the request's attribute
         request.setAttribute("BMI", BMI);
-        request.setAttribute("builtType", builtType())
+        request.setAttribute("builtType", builtType);
 
         //TODO: forward to jsp
         request.getRequestDispatcher("bmi_result.jsp").forward(request, response);
