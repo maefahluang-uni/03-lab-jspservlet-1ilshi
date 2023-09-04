b881409a1b81c9eca173910d2645623aa5d7be41
Muhammadrasheed Rida Ahmad Unus Ahmad Samadi <105810480+1ilshi@users.noreply.github.com>
2023-09-04 16:09:26 +0700

    not done yet

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -1,6 +1,7 @@
 package th.mfu;
 
 import java.io.IOException;
+import Math;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,19 +10,40 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //TODO: add webservlet to "/calbmi"
+@WebServlet(urlPatterns = "/calbmi")
 public class BMICalculatorServlet extends HttpServlet{
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //TODO: get parameter from request: "weight" and "height"
+        int weight = Integer.parseInt(request.getParameter("weight"));
+        int height = Integer.parseInt(request.getParameter("height"));
         
         //TODO: calculate bmi
+        int BMI = weight/(height*height);
+        BMI = Math.round(BMI);
 
         //TODO: determine the built from BMI
+        String builtType(){
+            if(BMI < 18.5){
+                return "underweight";
+            }else if(BMI >= 18.5 && BMI <25){
+                return "normal";
+            }else if(BMI >= 25 && BMI < 30){
+                return "overweight";
+            }else if(BMI >= 30 && BMI < 35){
+                return "obese";
+            }else if(BMI >= 35){
+                return "extremely obese"
+            }
+        }        
       
         //TODO: add bmi and built to the request's attribute
+        request.setAttribute("BMI", BMI);
+        request.setAttribute("builtType", builtType())
 
         //TODO: forward to jsp
+        request.getRequestDispatcher("bmi_result.jsp").forward(request, response);
            
     }
     
